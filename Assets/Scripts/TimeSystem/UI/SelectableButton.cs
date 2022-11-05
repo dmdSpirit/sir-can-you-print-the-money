1c8d3d0cdbd64774d5d1241bd3369150feb21ec4
ttv-dev-spirit <ttv.dev.spirit@gmail.com>
2022-11-05 17:24:00 +0700

    123 controls added, pause toggle fixed

---
+++ Assets/Scripts/TimeSystem/UI/SelectableButton.cs
@@ -0,0 +1,59 @@
+#nullable enable
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NovemberProject.TimeSystem.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class SelectableButton : MonoBehaviour
+    {
+        private readonly Subject<Unit> _onClicked = new();
+
+        [SerializeField]
+        private Color _selectedColor;
+
+        [SerializeField]
+        private Color _unselectedColor;
+
+        public Button Button { get; private set; } = null!;
+        public IObservable<Unit> OnClicked => _onClicked;
+
+        public bool IsSelected { get; private set; }
+
+        private void Awake()
+        {
+            Button = GetComponent<Button>();
+            Button.OnClickAsObservable().Subscribe(OnButtonClicked);
+        }
+
+        private void OnButtonClicked(Unit _)
+        {
+            Select();
+            _onClicked.OnNext(Unit.Default);
+        }
+
+        public void Select()
+        {
+            if (IsSelected)
+            {
+                return;
+            }
+
+            Button.targetGraphic.color = _selectedColor;
+            IsSelected = true;
+        }
+
+        public void Unselect()
+        {
+            if (!IsSelected)
+            {
+                return;
+            }
+
+            Button.targetGraphic.color = _unselectedColor;
+            IsSelected = false;
+        }
+    }
+}
